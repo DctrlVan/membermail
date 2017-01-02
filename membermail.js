7430b369cd8e8967555acaee0282a303c717ce51
Kiko Marrs <findkiko@gmail.com>
2017-01-02 14:59:36 -0800

    Shove it all in function :)

--- membermail.js
+++ membermail.js
@@ -9,45 +9,49 @@ var sg   = require('sendgrid')(process.env.SENDGRID_TOKEN);
 var template = require('./template');
 var emailBody = template.templateReader("template.html");
 
-for (i = 0; i < members.length; i++) {
-  member = members[i];
+function sendEmails() {
+  for (i = 0; i < members.length; i++) {
+    member = members[i];
 
-  helper = require('sendgrid').mail;
-  mail = new helper.Mail();
+    helper = require('sendgrid').mail;
+    mail = new helper.Mail();
 
-  from_email = new helper.Email(metadata.from);
-  mail.setFrom(from_email);
+    from_email = new helper.Email(metadata.from);
+    mail.setFrom(from_email);
 
-  mail.setSubject(metadata.subject);
+    mail.setSubject(metadata.subject);
 
-  personalization = new helper.Personalization();
+    personalization = new helper.Personalization();
 
-  to_email = new helper.Email(member.Email);
-  personalization.addTo(to_email);
+    to_email = new helper.Email(member.Email);
+    personalization.addTo(to_email);
 
-  substitution = new helper.Substitution("%firstName%", member.First);
-  personalization.addSubstitution(substitution);
+    substitution = new helper.Substitution("%firstName%", member.First);
+    personalization.addSubstitution(substitution);
 
-  substitution = new helper.Substitution("%amount%", metadata.amount);
-  personalization.addSubstitution(substitution);
+    substitution = new helper.Substitution("%amount%", metadata.amount);
+    personalization.addSubstitution(substitution);
 
-  substitution = new helper.Substitution("%addr%", member.BTCAddr);
-  personalization.addSubstitution(substitution);
+    substitution = new helper.Substitution("%addr%", member.BTCAddr);
+    personalization.addSubstitution(substitution);
 
-  mail.addPersonalization(personalization);
+    mail.addPersonalization(personalization);
 
-  content = new helper.Content('text/html', emailBody);
-  mail.addContent(content);
+    content = new helper.Content('text/html', emailBody);
+    mail.addContent(content);
 
-  request = sg.emptyRequest({
-          method: 'POST',
-          path: '/v3/mail/send',
-          body: mail.toJSON()
-  });
+    request = sg.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: mail.toJSON()
+    });
 
-  sg.API(request, function(error, response) {
-    console.log(response.statusCode);
-    console.log(response.body);
-    console.log(response.headers);
-  });
-}
+    sg.API(request, function(error, response) {
+      console.log(response.statusCode);
+      console.log(response.body);
+      console.log(response.headers);
+    });
+  }
+};
+
+sendEmails();
